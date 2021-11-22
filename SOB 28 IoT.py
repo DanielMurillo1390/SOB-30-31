6abda7df7fe51908c7dce65107e5466eacb43220
DanielMurillo1390 <94857449+DanielMurillo1390@users.noreply.github.com>
2021-11-22 17:08:28 +0000

    Update SOB 28 IoT.py
    Comments were added

--- SOB 28 IoT.py
+++ SOB 28 IoT.py
@@ -1,9 +1,13 @@
+# a is our main variable as it allow us to print the specific shape.
 a="*" 
+# n is the counter that will allow us to go from 0 to 5 and then form 5 to 0
 n=0
+# this FOR loop will print the first 5 rows
 for i in range (6):
    
     print(a*n)
     n+=1
+# this FOR loop will print the last 5 rows
 for i in range(6):
 
     print(a*n)
